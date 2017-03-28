3921cb9c73b9c94193b8b50723d85aaef14c8b62
Andrea <andre.kiba@gmail.com>
2017-03-28 08:03:12 +0200

    AkkaPlayground

---
+++ AkkaPlayground/Actors/PlaybackActor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Akka.Actor;
+using AkkaPlayground.Messages;
+
+namespace AkkaPlayground.Actors
+{
+    public class PlaybackActor : ReceiveActor
+    {
+        public PlaybackActor()
+        {
+            Console.WriteLine("Creating a PlaybackActor");
+
+            Receive<PlayMovieMessage>(HandlePlayMovieMessage, message => message.UserId == 43);
+        }
+
+        private static void HandlePlayMovieMessage(PlayMovieMessage message)
+        {
+            Console.WriteLine("Received movie title " + message.MovieTitle);
+            Console.WriteLine("Received user Id " + message.UserId);
+        }
+    }
+}
---
+++ AkkaPlayground/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Akka.Actor;
+using AkkaPlayground.Actors;
+using AkkaPlayground.Messages;
+
+namespace AkkaPlayground
+{
+    internal class Program
+    {
+        private static ActorSystem movieStreamingActorSystem;
+
+        private static void Main(string[] args)
+        {
+            movieStreamingActorSystem = ActorSystem.Create("MovieStreamingActorSystem");
+            Console.WriteLine("Actor system created");
+
+            var playbackActorProps = Props.Create<PlaybackActor>();
+
+            var playbackActorRef = movieStreamingActorSystem.ActorOf(playbackActorProps, "PlaybackActor");
+
+            playbackActorRef.Tell(new PlayMovieMessage("Akka.NET: The Movie", 43));
+
+            Console.ReadLine();
+
+            movieStreamingActorSystem.Terminate();
+        }
+    }
+}
