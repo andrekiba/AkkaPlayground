245a0a971e857ed1a6db23228da09469ff3e9591
Andrea <andre.kiba@gmail.com>
2017-06-07 13:45:11 +0200

    update

---
+++ AkkaPlayground.Test/TestControllerActorTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using Akka.TestKit.VsTest;
+using AkkaPlayground.Actors;
+using AkkaPlayground.Messages;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AkkaPlayground.Test
+{
+    [TestClass]
+    public class TestControllerActorTest : TestKit
+    {
+        [TestMethod]
+        public void ShouldHaveChild()
+        {
+            var controller = ActorOfAsTestActorRef<TestControllerActor>();            
+            controller.Tell(new CreateTestActor("Actor1"));
+
+            Assert.IsTrue(controller.UnderlyingActor.Children.Any(x => x.Key == "Actor1"));
+        }
+    }
+}
--- AkkaPlayground/Actors/TestControllerActor.cs
+++ AkkaPlayground/Actors/TestControllerActor.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing.Text;
 using System.Threading.Tasks;
 using Akka.Actor;
@@ -8,13 +9,14 @@ namespace AkkaPlayground.Actors
 {
     public class TestControllerActor : ReceiveActor
     {
+        public Dictionary<string, IActorRef> Children { get; } = new Dictionary<string, IActorRef>();
         public TestControllerActor()
-        {
-            
+        {            
             Receive<CreateTestActor>(m =>
             {
                 ColorConsole.WriteOrange($"The Sender of CreateTestActor is '{Sender}'");
-                Context.ActorOf(Props.Create(() => new TestActor(m.ActorName)), m.ActorName);
+                var child = Context.ActorOf(Props.Create(() => new TestActor(m.ActorName)), m.ActorName);
+                Children.Add(m.ActorName, child);
             });
 
             Receive<AsyncResponseMessage>(m => ColorConsole.WriteGreen($"Async result from {Sender}"));           
